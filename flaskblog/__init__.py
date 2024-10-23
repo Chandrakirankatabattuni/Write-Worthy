30091afc319e5cfe94bf300e1bdeec35874c8660
Chandrakirankatabattuni <115807714+Chandrakirankatabattuni@users.noreply.github.com>
2024-10-23 22:04:41 +0530

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -17,9 +17,7 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 #app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_USERNAME'] = 'chandrakiran.katabattuni@gmail.com'
 #app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-app.config['MAIL_PASSWORD'] = 'fdhw soue lili qmui'
 mail = Mail(app)
 
 from flaskblog import routes
